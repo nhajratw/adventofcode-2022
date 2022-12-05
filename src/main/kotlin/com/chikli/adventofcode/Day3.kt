a12a5c36262fb277c7f47d43ed976ccb51e8ef95
Nayan Hajratwala <nayan@chikli.com>
2022-12-04 21:13:35 -0500

    day 3 - part 2

--- src/main/kotlin/com/chikli/adventofcode/Day3.kt
+++ src/main/kotlin/com/chikli/adventofcode/Day3.kt
@@ -5,23 +5,25 @@ import java.io.FileReader
 fun main() {
     val inputData = FileReader("/tmp/day3.txt").readLines()
     val rucksacks = inputData.map { Rucksack(it) }
-    println(rucksacks.sumOf { it.commonItemPriority() })
+    var total = 0
+    for (i in rucksacks.indices step 3) {
+        val item = Rucksack.commonItem(rucksacks[i], rucksacks[i + 1], rucksacks[i + 2])
+        total += Rucksack.commonItemPriority(item)
+    }
+    println(total)
 }
 
-class Rucksack(private val items: String) {
-    fun commonItem(): Char {
-        val itemCount = items.length
-        val firstCompartment = items.substring(0, itemCount / 2)
-        val secondCompartment = items.substring(itemCount / 2, itemCount)
-        val common = firstCompartment.toSet().intersect(secondCompartment.toSet())
-        return common.first()
+class Rucksack(val items: String) {
+
+    companion object {
+
+        fun commonItem(first: Rucksack, second: Rucksack, third: Rucksack): Char {
+            return first.items.toSet().intersect(second.items.toSet()).intersect(third.items.toSet()).first()
+        }
+
+        fun commonItemPriority(item: Char): Int {
+            return if (item < 'a') item.code - 38
+            else item.code - 96
+        }
     }
-
-    fun commonItemPriority(): Int {
-        val item = commonItem()
-
-        return if (item < 'a') item.code - 38
-        else item.code - 96
-    }
-
 }
--- src/test/kotlin/com/chikli/adventofcode/Day3Test.kt
+++ src/test/kotlin/com/chikli/adventofcode/Day3Test.kt
@@ -1,27 +1,5 @@
 package com.chikli.adventofcode
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-
 class Day3Test {
 
-    @Test
-    fun `example items`() {
-        assertThat(Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp").commonItem()).isEqualTo('p')
-        assertThat(Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn").commonItem()).isEqualTo('v')
-        assertThat(Rucksack("ttgJtRGJQctTZtZT").commonItem()).isEqualTo('t')
-        assertThat(Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw").commonItem()).isEqualTo('s')
-        assertThat(Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL").commonItem()).isEqualTo('L')
-        assertThat(Rucksack("PmmdzqPrVvPwwTWBwg").commonItem()).isEqualTo('P')
-    }
-
-    @Test
-    fun `example priorities`() {
-        assertThat(Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp").commonItemPriority()).isEqualTo(16)
-        assertThat(Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn").commonItemPriority()).isEqualTo(22)
-        assertThat(Rucksack("ttgJtRGJQctTZtZT").commonItemPriority()).isEqualTo(20)
-        assertThat(Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw").commonItemPriority()).isEqualTo(19)
-        assertThat(Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL").commonItemPriority()).isEqualTo(38)
-        assertThat(Rucksack("PmmdzqPrVvPwwTWBwg").commonItemPriority()).isEqualTo(42)
-    }
 }
